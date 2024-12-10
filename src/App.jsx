bf84dbf4f2b8e622e111cd412e49707e6cc0c3f7
mdemiralay <m.demiralay@vitalen.com.tr>
2024-12-10 11:42:18 +0300

    featured section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
 
 const App = () => {
   return (
     <div>
      <Navbar/>
      <HeroSection/>
+     <FeatureSection/>
     </div>
   )
 }
